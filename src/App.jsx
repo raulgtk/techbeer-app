df8817d9f6c7df92411dde55b0c933681e0f5140
Raúl López <raul@trevenque.es>
2022-11-23 18:47:55 +0100

    techbeer logo added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import logoTrevenque from './assets/logo.svg'
-import techBeer from './assets/techbeer.png'
+import logoTechBeer from './assets/logo.png'
 import './App.css'
 
 function App() {
   const version = 1
   return (
     <div className="App">
-      <img src={logoTrevenque} alt="Trevenque" />
+      <img src={logoTechBeer} alt="Trevenque" />
       <h1>TechAndBeer #{version}</h1>
     </div>
   )
